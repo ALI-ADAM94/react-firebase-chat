f74af4da4fa105919607f30d1d648e9d15567b31
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-14 11:18:40 +0100

    First deploy to Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/react-firebase-chat"
+
 })
+//   base:"/react-firebase-chat"
+//   "homepage": "https://ALI-ADAM94.github.io/react-firebase-chat/",
+//       "predeploy":"npm run build",
+// "deploy":"gh-pages -d dist"
